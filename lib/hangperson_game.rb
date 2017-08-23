45f9d57b4b73abd7f6631a14fe78fe0500a0646b
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2017-08-23 16:24:17 +0300

    Finished Part 1 Testing

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,15 @@ class HangpersonGame
 
   # def initialize()
   # end
+
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
 
   def self.get_random_word
@@ -17,6 +23,48 @@ class HangpersonGame
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
+  end
+
+  def guess(letter)
+    letter = letter.to_s.downcase
+    if letter.empty?
+      raise ArgumentError 
+    elsif  !(letter =~ /[[:alpha:]]/)
+      raise ArgumentError
+    end
+    unless @guesses.count(letter) > 0 || @wrong_guesses.count(letter) > 0
+      unless @word.count(letter) > 0
+        @wrong_guesses << letter
+      else
+        @guesses << letter
+      end
+      true
+    else
+      false
+    end
+  end
+
+  def word_with_guesses
+    str = ""
+    @word.split("").each do |letter|
+      if @guesses.include? letter
+        str += letter
+      else
+        str += "-"
+      end
+    end
+    str
+  end
+
+  def check_win_or_lose
+    result = word_with_guesses
+    if result.eql?(@word)
+      :win
+    elsif @wrong_guesses.length >= 7
+      :lose
+    else
+      :play
+    end
   end
 
 end
